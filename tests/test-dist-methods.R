7b1b843e0b2026209d1dab9b8d08082f2f5285c0
EdCostaBR <64275913+EdCostaBR@users.noreply.github.com>
2025-11-17 14:34:14 -0300

    Create test-dist-methods.R

---
+++ tests/test-dist-methods.R
@@ -0,0 +1,17 @@
+library("podacc")
+
+data = amiodarone_tggates_29day_BMD
+
+head(data)
+
+list_bmd_values = data[,"BMD"]
+
+
+results = runDistMethods(list_bmd_values)
+
+# Examples how to plot results
+plotDistMethodResults(list_bmd_values,results)
+
+plotDistMethodResults(list_bmd_values,results,titleplot="DEMO Accumulation Plot Results",xlab_text = "BMD (mg/kg/day)")
+
+plotDistMethodResults(list_bmd_values,results,titleplot="DEMO Accumulation Plot Results",xlab_text = "BMD (mg/kg/day)",GOBP=50)
